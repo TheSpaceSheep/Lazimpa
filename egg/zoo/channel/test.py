df83983d6a6aeba378880b1d615434c6ba0feb3a
TheSpaceSheep <noe.malais@gmail.com>
2022-03-31 10:46:35 +0200

    fix bug

--- egg/zoo/channel/test.py
+++ egg/zoo/channel/test.py
@@ -255,8 +255,8 @@ def main(params):
         #                                         opts.receiver_hidden, cell=opts.receiver_cell,
         #                                         num_layers=opts.receiver_num_layers, max_len=opts.max_len, n_features=opts.n_features)
 
-    print(sender.state_dict)
     sender.load_state_dict(torch.load(opts.sender_weights,map_location=torch.device('cpu')))
+    print('test')
     weights = torch.load(opts.receiver_weights,map_location=torch.device('cpu'))
     weights['hidden_to_output.weight'] = weights['agent.output.weight']
     weights['hidden_to_output.bias'] = weights['agent.output.bias']
